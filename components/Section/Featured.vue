03c13b9d6e7aff63aa42bea0a130cbe02ebecdb1
Shirsendu Bairagi <shirsendu2001@gmail.com>
2024-01-11 00:25:18 +0530

    feat: design upgraded

---
+++ components/Section/Featured.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+import { breakpointsTailwind } from '@vueuse/core'
+
+const images = [
+	'Ceremony-006-001', 'Ceremony-006-002', 'Ceremony-006-003',
+	'Ceremony-006-004', 'Ceremony-006-005', 'Ceremony-006-006',
+	'Ceremony-006-007', 'Ceremony-006-008', 'Group-001-001',
+	'Group-001-002', 'Group-001-003', 'Group-002-001',
+	'Group-003-001', 'Group-004-001', 'Group-005-001',
+	'Group-006-001', 'Group-006-002', 'Portrait-001-001',
+	'Portrait-002-001', 'Portrait-003-001', 'Portrait-004-001',
+	'Portrait-005-001', 'Portrait-006-001', 'Portrait-007-001',
+	'Portrait-008-001', 'Portrait-009-001', 'Portrait-010-001',
+	'Portrait-011-001', 'Portrait-012-001', 'Portrait-013-001',
+	'Portrait-014-001', 'Portrait-015-001', 'Portrait-016-001',
+	'Wedding-001-001', 'Wedding-001-002', 'Wedding-001-003',
+	'Wedding-001-004', 'Wedding-001-005', 'Wedding-001-006',
+	'Wedding-001-007', 'Wedding-001-008', 'Wedding-001-009',
+	'Wedding-001-010', 'Wedding-001-011', 'Wedding-001-012',
+	'Wedding-001-013', 'Wedding-001-014'
+]
+
+const breakpoints = useBreakpoints(breakpointsTailwind)
+const mdDevices = breakpoints.greaterOrEqual('sm')
+
+const container = ref()
+const offset = useInterval(25)
+
+const { y } = useScroll(container, { behavior: 'smooth' })
+
+watch(y, (value) => {
+	offset.value = value
+})
+
+watch(offset, (value) => {
+	y.value = value
+})
+
+const imageSlides = computed(() => {
+	const noOfSlides = !mdDevices.value ? 2 : 3
+	const slides: string[][] = new Array(noOfSlides).fill(null).map(_ => [])
+
+	images.forEach((image, index) => {
+		slides[index % noOfSlides].push(image)
+	});
+
+	return slides
+})
+</script>
+
+<template>
+	<section ref="container" id="featured" class="relative mx-0 md:-mx-12 max-h-screen overflow-y-scroll scrollbar-hidden">
+		<div class="flex gap-2 md:gap-4">
+			<ClientOnly>
+				<div v-for="images, index in imageSlides" :key="index" class="flex flex-col gap-2 md:gap-4"
+					:class="{ 'translate-y-5': index == 0, '-translate-y-4': index == 1, 'translate-y-12': index == 2 }">
+					<img v-for="image in images" :key="image" :src="`/images/photos/${image}.webp`" :alt="image"
+						class="w-full object-cover rounded-sm md:rounded-md" />
+				</div>
+			</ClientOnly>
+		</div>
+	</section>
+</template>
+
+<style scoped>
+.overlay {
+	@apply after:content-[''] after:fixed after:left-0 after:top-0 after:w-screen after:h-screen after:bg-gradient-to-b after:from-black/40 after:via-transparent after:to-black/40 after:from-[3%] after:via-20% after:to-[97%] after:z-20
+}
+</style>
---
+++ components/Section/Hero.vue
@@ -0,0 +1,16 @@
+<template>
+	<section id="hero"
+		class="relative grid grid-rows-[repeat(3,min-content)] md:grid-rows-1 grid-cols-3 items-center gap-y-[4.5rem] mt-24 md:m-0 min-h-screen">
+		<div class="row-start-1 !col-start-1 col-span-3 md:col-span-2 flex flex-col gap-6 text-center md:text-left">
+			<h1 class="text-3xl md:text-5xl font-semi-bold">Capture the <br />memorable moments</h1>
+			<p class="text-lg md:text-xl leading-9 tracking-wide">Moments are too precious, <br />it should be chariest life
+				long</p>
+			<CTAButton class="hidden md:flex" />
+		</div>
+		<div class="relative row-start-2 md:row-start-1 col-start-1 md:col-start-2 col-span-3 md:col-span-2 -z-10">
+			<NuxtImg src="/images/background.webp" alt="background" width="1200"
+				class="relative right-0 bottom-0 -translate-y-[10%] -translate-x-[25%] md:-translate-x-[10%] scale-[165%] md:scale-125" />
+		</div>
+		<CTAButton class="md:hidden row-start-3 col-start-2 justify-self-center items-center" />
+	</section>
+</template>
